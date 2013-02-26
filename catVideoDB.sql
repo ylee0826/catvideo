638052ff5ca56e0617cb64bd192ffe7897e3427b
Erica Mathews <emathews@mail.umw.edu>
2013-02-26 14:15:57 -0500

    Added a video to the table

--- catVideoDB.sql
+++ catVideoDB.sql
@@ -24,4 +24,5 @@ INSERT INTO videos (title, url, vidlength, breed, category, tags, rating) VALUES
 ('ROOMBA Cat uses iRobot Roomba 560 Robotic Vacuum Cleaner', 'http://www.youtube.com/watch?feature=player_embedded&v=ewdbilSWjaM', '00:00:59', '', 'Funny', 'roomba', 4.0),
 ('Surprised Kitty', 'http://www.youtube.com/watch?feature=player_embedded&v=0Bmhjf0rKe8', '00:00:17', 'Shorthair', 'Cute', 'supprised', 9.0),
 ('Cute cat thinks it\'s a dog', 'http://www.youtube.com/watch?feature=player_embedded&v=8ZuCT4PmurY#!', '00:01:09', 'Shorthair', 'Funny', 'Dog', 6.0),
+('Epic Cat Tower Battle', 'https://www.youtube.com/watch?v=33uTpyH_OFY', '00:01:59', 'Shorthair', 'Funny', 'fighting, biting, climbing, playing', 7.9),
 ('Cat barks like a dog at bubbles', 'http://www.youtube.com/watch?v=IRxX6LZmr2U&list=FL15i_L_4S6E52-ysZ9AD76Q', '00:00:55', 'Shorthair', 'Funny', 'bubbles, playing, meowing', 8.5);
