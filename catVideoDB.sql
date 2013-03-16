7e3528249f033a4af3062eaa1338586308948c7a
Erica Mathews <emathews@mail.umw.edu>
2013-03-16 11:24:59 -0400

    Revamped database design
    Added tables to make sure the "videos" table was 3NF. Still need to
    insert data into each table. Saved all of the old data into a file
    called "catVideoBDOld.sql"

--- catVideoDB.sql
+++ catVideoDB.sql
@@ -1,5 +1,3 @@
-
-
 CREATE DATABASE IF NOT EXISTS catvideos;
 GRANT ALL PRIVILEGES ON catvideos.* to 'ylee'@'localhost' identified by 'hello123';
 USE catvideos;
@@ -7,22 +5,48 @@ USE catvideos;
 DROP TABLE IF EXISTS videos;
 
 CREATE TABLE IF NOT EXISTS videos (
-  id int NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(50) NOT NULL,
   url BLOB NOT NULL,
   vidlength TIME,
-  breed VARCHAR(25),
-  category VARCHAR(25),
-  tags BLOB,
   rating DEC(3,1),
   PRIMARY KEY (id)
 ) ;
 
-INSERT INTO videos (title, url, vidlength, breed, category, tags, rating) VALUES
-('Many too small boxes and Maru', 'https://www.youtube.com/watch?v=2XID_W4neJo', '00:02:58', 'Scottish fold', 'Funny', 'box, playing', 8.2),
-('Cat wants to be petted like this', 'http://www.youtube.com/watch?feature=player_embedded&v=V6JCaT69v1M', '00:00:10', 'Shorthair', 'heatwarming', 'petted', 5.0),
-('ROOMBA Cat uses iRobot Roomba 560 Robotic Vacuum Cleaner', 'http://www.youtube.com/watch?feature=player_embedded&v=ewdbilSWjaM', '00:00:59', '', 'Funny', 'roomba', 4.0),
-('Surprised Kitty', 'http://www.youtube.com/watch?feature=player_embedded&v=0Bmhjf0rKe8', '00:00:17', 'Shorthair', 'Cute', 'supprised', 9.0),
-('Cute cat thinks it\'s a dog', 'http://www.youtube.com/watch?feature=player_embedded&v=8ZuCT4PmurY#!', '00:01:09', 'Shorthair', 'Funny', 'Dog', 6.0),
-('Epic Cat Tower Battle', 'https://www.youtube.com/watch?v=33uTpyH_OFY', '00:01:59', 'Shorthair', 'Funny', 'fighting, biting, climbing, playing', 7.9),
-('Cat barks like a dog at bubbles', 'http://www.youtube.com/watch?v=IRxX6LZmr2U&list=FL15i_L_4S6E52-ysZ9AD76Q', '00:00:55', 'Shorthair', 'Funny', 'bubbles, playing, meowing', 8.5);
+
+DROP TABLE IF EXISTS breeds;
+
+CREATE TABLE IF NOT EXISTS breeds (
+	breed_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	breed_name VARCHAR(30)
+);
+
+
+DROP TABLE IF EXISTS tags;
+
+CREATE TABLE IF NOT EXISTS tags (
+	tag_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	tag_name VARCHAR(30)
+);
+
+
+DROP TABLE IF EXISTS vid_breeds;
+
+CREATE TABLE IF NOT EXISTS vid_breeds (
+	id INT NOT NULL,
+	breed_id INT NOT NULL,
+	FOREIGN KEY (id) REFERENCES videos(id),
+	FOREIGN KEY (breed_id) REFERENCES breeds(breed_id),
+	PRIMARY KEY (id, breed_id)
+);
+
+
+DROP TABLE IF EXISTS vid_tags;
+
+CREATE TABLE IF NOT EXISTS vid_tags (
+	id INT NOT NULL,
+	tag_id INT NOT NULL,
+	FOREIGN KEY (id) REFERENCES videos(id),
+	FOREIGN KEY (tag_id) REFERENCES tags(tag_id),
+	PRIMARY KEY (id, tag_id)
+);
