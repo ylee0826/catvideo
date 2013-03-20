da48aaea61b77c495e941ce78923087132defc62
ylee <ylee0826@gmail.com>
2013-03-20 16:51:52 -0400

    changed the search options

--- search.php
+++ search.php
@@ -25,27 +25,63 @@
 					
 					<form method = "post" action = "search2.php">
 					<table>
-					<tr><td><img src="cat1.png"/></td><td><input type="text" id="search" name="search" size="70"/></td>
+					<tr><td><img src="cat1.png"/></td><td><input type="text" id="search" name="search" size="50"/></td>
 					<td>&nbsp;</td><td><input type="submit" value="Search" /></td></tr>
 					
-					<tr><td>Check the breed you want to look at:</td><td>&nbsp;</td></tr>
-					<tr><td><input type="checkbox" name="Unspecified" value="checked" /> Unspecified</td><td><input type="checkbox" name="Persian" value="checked" /> Persian</td></tr>
-					<tr><td><input type="checkbox" name="Exotic shorthair" value="checked" /> Exotic shorthair</td><td><input type="checkbox" name="British shorthair" value="checked" /> British shorthair</td></tr>
-					<tr><td><input type="checkbox" name="Siamese" value="checked" /> Siamese</td><td><input type="checkbox" name="Ragdoll" value="checked" /> Ragdoll</td></tr>
-					<tr><td><input type="checkbox" name="Bengal" value="checked" /> Bengal</td><td><input type="checkbox" name="Tabby" value="checked" /> Tabby</td></tr>
-					<tr><td><input type="checkbox" name="Scottish fold" value="checked" /> Scottish fold</td><td><input type="checkbox" name="Hairless" value="checked" /> Hairless</td></tr>
+					<tr><td>&nbsp;</td><td>&nbsp;</td></tr>
+					<tr><td>&nbsp;</td><td>Choose breed you want to search by: </td></tr>
+					<tr><td>&nbsp;</td><td>
+					<form action="">
+						<select name="breed">
+							<option value="unspecified">unspecified</option>
+							<option value="exoticShorthair">exotic shorthair</option>
+							<option value="siamese">siamese</option>
+							<option value="bengal">bengal</option>
+							<option value="scottishFold">scottish fold</option>
+							<option value="persian">persian</option>
+							<option value="britishShorthair">british shorthair</option>
+							<option value="ragdoll">ragdoll</option>
+							<option value="tabby">tabby</option>
+							<option value="hairless">hairless</option>
+						</select>
+					</form>
+					</td>
+					</tr>
 					
-					<tr><td>Check the tag you are interested in:</td><td>&nbsp;</td></tr>
-					<tr><td><input type="checkbox" name="cute" value="checked" /> cute</td><td><input type="checkbox" name="funny" value="checked" /> funny</td></tr>
-					<tr><td><input type="checkbox" name="heartwarming" value="checked" /> heartwarming</td><td><input type="checkbox" name="awesome" value="checked" /> awesome</td></tr>
-					<tr><td><input type="checkbox" name="playing" value="checked" /> playing</td><td><input type="checkbox" name="sleeping" value="checked" /> sleeping</td></tr>
-					<tr><td><input type="checkbox" name="cuddling" value="checked" /> cuddling</td><td><input type="checkbox" name="fighting" value="checked" /> fighting</td></tr>
-					<tr><td><input type="checkbox" name="biting" value="checked" /> biting</td><td><input type="checkbox" name="vocalizing" value="checked" /> vocalizing</td></tr>
-					<tr><td><input type="checkbox" name="chasing" value="checked" /> chasing</td><td><input type="checkbox" name="jumping" value="checked" /> jumping</td></tr>
-					<tr><td><input type="checkbox" name="eating" value="checked" /> eating</td><td><input type="checkbox" name="hissing" value="checked" /> hissing</td></tr>
-					<tr><td><input type="checkbox" name="climbing" value="checked" /> climbing</td><td><input type="checkbox" name="acting like a dog" value="checked" /> acting like a dog</td></tr>
-					<tr><td><input type="checkbox" name="being pet" value="checked" /> being pet</td><td><input type="checkbox" name="kitten" value="checked" /> kitten</td></tr>
-					<tr><td><input type="checkbox" name="doing tricks" value="checked" /> doing tricks</td></tr>
+					<tr><td>&nbsp;</td><td>&nbsp;</td></tr>
+					<tr><td>&nbsp;</td><td>or</td><tr>
+					<tr><td>&nbsp;</td><td>&nbsp;</td></tr>
+					<tr><td>&nbsp;</td><td>Choose tag you want to search by:</td></tr>
+					<tr><td>&nbsp;</td><td>
+					
+					<form action="">
+						<select name="tags">
+							<option value="cute">cute</option>
+							<option value="heartwarming">heartwarming</option>
+							<option value="">playing</option>
+							<option value="">cuddling</option>
+							<option value="">biting</option>
+							<option value="">chasing</option>
+							<option value="">eating</option>
+							<option value="">climbing</option>
+							<option value="">being pet</option>
+							<option value="">doing tricks</option>
+							<option value="">funny</option>
+							<option value="">awesome</option>
+							<option value="">sleeping</option>
+							<option value="">fighting</option>
+							<option value="">vocalizing</option>
+							<option value="">jumping</option>
+							<option value="">hissing</option>
+							<option value="">acting like a dog</option>
+							<option value="">kitten</option>
+							<option value="">doing tricks</option>
+					<tr><td>&nbsp;</td><td>&nbsp;</td></tr>
+							
+						</select>
+					</form>				
+					</td></tr>
+
 					</table>
 					</form>
 					<!-- END CONTENT -->
