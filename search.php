ca6a457d3ee584a9d26f399855bbb514cfc2472c
Erica Mathews <emathews@mail.umw.edu>
2013-03-20 15:16:34 -0400

    Put search files back so we can edit and use them

---
+++ search.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Cat Video</title>
+<link rel="stylesheet" href="styles.css" type="text/css" />
+
+
+</head>
+<body>
+<?php
+   include('header.html');
+?>
+
+
+<div id="wrap">
+	<div class="pagewrapper">
+		<div class="innerpagewrapper">
+			<div class="page">
+				<div class="content">
+				
+					<!-- CONTENT -->
+					<h3>Cat Video</h3>
+					<p>Search for a video you want to watch</p>
+					
+					<form method = "post" action = "search2.php">
+					<table>
+					<tr><td><img src="cat1.png"/></td><td><input type="text" id="search" name="search" size="70"/></td>
+
+					<td>&nbsp;</td><td><input type="submit" value="Search" /></td></tr>
+					</table>
+					</form>
+					<!-- END CONTENT -->
+					
+				</div>
+				
+				<?php
+				    include('SIDEnFOOTER.html');
+				?>
+				   
+
+
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
---
+++ search2.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Cat Video</title>
+<link rel="stylesheet" href="styles.css" type="text/css" />
+
+
+</head>
+<body>
+<?php
+   include('header.html');
+?>
+
+
+<div id="wrap">
+	<div class="pagewrapper">
+		<div class="innerpagewrapper">
+			<div class="page">
+				<div class="content">
+				
+					<!-- CONTENT -->
+					<h3>Search Result:</h3>
+					<table>
+					<tr><th>Title</th><th>URL</th><th>Length</th><th>Rating</th></tr>
+
+					<?php
+					include('dbconnect.php');
+					$searchValue = $_POST['search'];
+
+					$query = "SELECT title,url,vidlength,rating FROM videos WHERE title LIKE '%".$searchValue. "%'";
+					$result = mysqli_query($db, $query)
+							or die("Error Querying Database");
+					while($row = mysqli_fetch_array($result)){
+						$title = $row['title'];
+						$url = $row['url'];
+						$vidlength = $row['vidlength'];
+						$rate = $row['rating'];
+						echo "<tr><td>$title</td><td>$url</td><td>$vidlength</td><td>$rate</td></tr>\n";	
+					}
+					
+					mysqli_close($db);
+					echo "<p>$searchValue</p>";
+					
+
+
+					
+					?>
+					</table>
+					
+					
+					</form>
+					<!-- END CONTENT -->
+					
+				</div>
+				
+				<?php
+				    include('SIDEnFOOTER.html');
+				?>
+				   
+
+
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
